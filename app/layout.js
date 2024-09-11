a391f44ab0f911ec7c5758e4b74df1aa8cef2eae
Ashish Deshmukh674 <deshmukhashish674@gmail.com>
2024-09-12 00:47:45 +0530

    sixth commit

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ratnamukund HealthCare Foundation",
-  description: "",
+  description: "Dr. Bhat has served as a Senior Research Associate at the renowned John Hopkins University and has made significant contributions as an Assistant Professor of Medicine at Bharatratna Atalbihari Vajpayee Medical College, Pune. He has also shared his expertise as an Honorary Physician with Pune Municipal Corporation and as a consulting physician at Tata Motors, Pune",
 };
 
 export default function RootLayout({ children }) {
