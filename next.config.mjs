eb4ab0a403f5d953ea80488669da33f5475130e5
Ashish Deshmukh674 <deshmukhashish674@gmail.com>
2024-09-13 03:48:09 +0530

    26rd commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
     
     reactStrictMode:false,
+    async rewrites() {
+        return [
+          {
+            source: '/sitemap.xml',
+            destination: '/site/sitemap', // Adjust to the path of the API route
+          },
+        ];
+      },
     swcMinify: true,
     images:{
         domains:['res.cloudinary.com','lh3.googleusercontent.com']
