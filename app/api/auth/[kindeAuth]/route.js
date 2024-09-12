dc288fba380d686c9e69c6eaae13038681e983ae
Ashish Deshmukh674 <deshmukhashish674@gmail.com>
2024-09-12 14:37:37 +0530

    10th commit

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,19 +1,2 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-
-export const GET = async (req, res) => {
-  try {
-    // Handle authentication and redirect after login
-    await handleAuth(req, res, {
-      afterCallback: (req, res, session) => {
-        console.log("User successfully authenticated:", session);
-        // Redirect to a specific route after successful login
-        res.redirect('/appointments');  // Redirect to your desired page
-      },
-    });
-  } catch (error) {
-    console.error('Error during authentication:', error);
-
-    // Return a proper error response to help with debugging
-    res.status(500).json({ error: 'Authentication failed. Please try again.' });
-  }
-};
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,8 @@ const nextConfig = {
     reactStrictMode:false,
     images:{
         domains:['res.cloudinary.com','lh3.googleusercontent.com']
-    },
-    env: {
-        KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
-        KINDE_DOMAIN: process.env.KINDE_DOMAIN,
-        KINDE_SECRET: process.env.KINDE_SECRET,
-      }
+    }
+    
 };
 
 export default nextConfig;
